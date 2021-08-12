acef8df5c8f6f73bf13a2668c2f24e2352c3c3d3
Nikhil Musale <nikhil.musale@morningstar.com>
2021-08-12 15:32:19 +0530

    [Nikhil] - Added months condition for uc5

--- src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
+++ src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
@@ -11,27 +11,33 @@ public class GamblerSimulation {
     public static void main(String[] args) {
 
         int day = 1;
+        int month =1;
         int totalMoney=dailyStake;
+        while (month<=12) {
+            System.out.println("\n\tFor month  " + month + "\n");
+            day=1;
+            while (day <= 30) {
+                Random ran = new Random();
+                totalMoney=dailyStake;
+                int dailyGamePlayed = ran.nextInt(10);
 
-        while(day<=20){
-            Random ran = new Random();
-            int dailyGamePlayed = ran.nextInt(10);
+                for (int i = 0; i < dailyGamePlayed; i++) {
 
-            for (int i=0; i<dailyGamePlayed ;i++) {
+                    if (winLoose() == 1)            //for win
+                        totalMoney = totalMoney + 1;
 
-                if (winLoose() == 1)            //for win
-                    totalMoney = totalMoney + 1;
+                    else                            //for loss
+                        totalMoney = totalMoney - 1;
 
-                else                            //for loss
-                    totalMoney = totalMoney - 1;
-
+                }
+                if (totalMoney < dailyStake)
+                    System.out.println("Day " + day + " Lost by :- " + (dailyStake - totalMoney) + " Dollar");
+                else
+                    System.out.println("Day " + day + " won by :- " + (totalMoney - dailyStake) + " Dollar");
+                day++;
             }
-            day++;
+            month++;
         }
-        if (totalMoney<dailyStake)
-            System.out.println("After 20 days you have lost "+ (dailyStake - totalMoney)+ " Dollar");
-        else
-            System.out.println("After 20 days you have won "+ (totalMoney - dailyStake)+ " Dollar");
     }
 
     public static double StakePercent(){
