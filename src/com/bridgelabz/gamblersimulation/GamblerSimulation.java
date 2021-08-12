ad9a1774aa8a82784d2a5afb703db5825d412b97
Nikhil Musale <nikhil.musale@morningstar.com>
2021-08-12 15:17:39 +0530

    [Nikhil] - Added 20 days condition for uc4

--- src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
+++ src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
@@ -10,20 +10,30 @@ public class GamblerSimulation {
 
     public static void main(String[] args) {
 
+        int day = 1;
         int totalMoney=dailyStake;
-        do {
-            if (winLoose() == 1) {            //for win
-                totalMoney = totalMoney + 1;
-                System.out.println("Congratulations you won" );
-            } else {                           //for loss
-                totalMoney = totalMoney - 1;
-                System.out.println("Unfortunately you loose, Better luck next time" );
-            }
-        }while(totalMoney> dailyStake - dailyStake*StakePercent() && totalMoney< dailyStake + dailyStake*StakePercent());
 
-        System.out.println("Resiging with "+totalMoney+ " money");
+        while(day<=20){
+            Random ran = new Random();
+            int dailyGamePlayed = ran.nextInt(10);
+
+            for (int i=0; i<dailyGamePlayed ;i++) {
+
+                if (winLoose() == 1)            //for win
+                    totalMoney = totalMoney + 1;
+
+                else                            //for loss
+                    totalMoney = totalMoney - 1;
+
+            }
+            day++;
+        }
+        if (totalMoney<dailyStake)
+            System.out.println("After 20 days you have lost "+ (dailyStake - totalMoney)+ " Dollar");
+        else
+            System.out.println("After 20 days you have won "+ (totalMoney - dailyStake)+ " Dollar");
     }
-    
+
     public static double StakePercent(){
         double percentStake = percentage/100.0;
         return percentStake;
