643dae5cbcce02416c76af58b78cda4a5ce1c94b
Nikhil Musale <nikhil.musale@morningstar.com>
2021-08-12 12:04:39 +0530

    [Nikhil] - Added win loose for uc2

--- src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
+++ src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
@@ -1,10 +1,26 @@
 package com.bridgelabz.gamblersimulation;
 
+import java.util.Random;
+
 public class GamblerSimulation {
+
     public static final int dailyStake=200;
     public static final int bet=1;
 
     public static void main(String[] args) {
 
+        int totalMoney;
+        if (winLoose()==1) //for win
+            totalMoney = dailyStake+1;
+        else                // for loss
+            totalMoney = dailyStake-1;
+
     }
+
+    public static int winLoose(){
+        Random ran = new Random();
+        int result = ran.nextInt(2);
+        return result;
+    }
+
 }
