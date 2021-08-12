c527a72a23b6d9fea51e6eb4cc5e03e8cdaf9d23
Nikhil Musale <nikhil.musale@morningstar.com>
2021-08-12 11:55:13 +0530

    [Nikhil] - Added daily stake and every game fee

---
+++ src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.gamblersimulation;
+
+public class GamblerSimulation {
+    public static final int dailyStake=200;
+    public static final int bet=1;
+
+    public static void main(String[] args) {
+
+    }
+}
