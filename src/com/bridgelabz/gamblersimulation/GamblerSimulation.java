161c032afae7ccb388a444671de229004401da01
Nikhil Musale <nikhilmusale70@gmail.com>
2021-08-13 00:06:49 +0530

    added luckday unlucky day feature for uc6

--- src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
+++ src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
@@ -13,8 +13,15 @@ public class GamblerSimulation {
         int day = 1;
         int month =1;
         int totalMoney=dailyStake;
+        int unluckyDay=1;
+        int luckyDay=1;
+        int unluckyMonth=1;
+        int luckyMonth=1;
+        int unluckyDayMoney=dailyStake;
+        int luckyDayMoney = dailyStake;
+
         while (month<=12) {
-            System.out.println("\n\tFor month  " + month + "\n");
+
             day=1;
             while (day <= 30) {
                 Random ran = new Random();
@@ -25,19 +32,29 @@ public class GamblerSimulation {
 
                     if (winLoose() == 1)            //for win
                         totalMoney = totalMoney + 1;
-
                     else                            //for loss
                         totalMoney = totalMoney - 1;
+                }
+                if (unluckyDayMoney > totalMoney){
+                    unluckyDay=day;
+                    unluckyMonth=month;
+                    unluckyDayMoney = totalMoney;
 
                 }
-                if (totalMoney < dailyStake)
-                    System.out.println("Day " + day + " Lost by :- " + (dailyStake - totalMoney) + " Dollar");
-                else
-                    System.out.println("Day " + day + " won by :- " + (totalMoney - dailyStake) + " Dollar");
+                if(luckyDayMoney < totalMoney){
+                    luckyDay=day;
+                    luckyMonth=month;
+                    luckyDayMoney = totalMoney;
+                }
+
                 day++;
             }
             month++;
         }
+
+        System.out.println("Unlucly day :- " + unluckyDay + " Unlucky month :- " + unluckyMonth + " lost "+ (dailyStake-unluckyDayMoney) + "dollars");
+
+        System.out.println("lucly day :- " + luckyDay + " lucky month :- " + luckyMonth + " won "+ (luckyDayMoney-dailyStake) + " dollars");
     }
 
     public static double StakePercent(){
